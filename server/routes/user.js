63bde1efa70eaa0df9aed7d96488a35759c35e46
sakurakiyama <sakura.bowden@gmail.com>
2023-09-26 00:08:35 +0200

    add new route handlers

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 import { Router } from 'express';
 import userController from '../controllers/userController/index.js';
 const router = Router();
@@ -14,4 +10,23 @@ router.post('/createUser', userController.createUser, (req, res) => {
   return res.status(200).json(res.locals.user);
 });
 
+router.post('/createItinerary', userController.createItinerary, (req, res) => {
+  return res.status(200).json(res.locals.newItinerary);
+});
+
+router.get('/getItineraries/:id', userController.getItineraries, (req, res) => {
+  return res.status(200).json(res.locals.itineraries);
+});
+
+router.post('/createNewCard', userController.createNewCard, (req, res) => {
+  return res.status(200).json(res.locals.newCard);
+});
+
+router.patch('/updateCard', userController.updateCard, (req, res) => {
+  return res.status(200).json(res.locals.updatedCard);
+});
+
+router.patch('/updateItinerary', userController.updateItinerary, (req, res) => {
+  return res.status(200).json(res.locals.itinerary);
+});
 export default router;
