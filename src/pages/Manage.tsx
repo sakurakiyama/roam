f4dd68f1e37e6098784000db1b110aa0e7db39cd
sakurakiyama <sakura.bowden@gmail.com>
2023-09-26 00:07:49 +0200

    add page to manage itineraries - unstyled

--- src/pages/Manage.tsx
+++ src/pages/Manage.tsx
@@ -1,14 +1,33 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import Nav from '../components/shared/Nav';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../utils/UserProvider';
+import { UserData } from '../types';
 
+/* 
+
+TODO: [] Add a modal to navigate users to login if not signed in. 
+TODO: [] Add styling 
+TODO: [] Add logic to click on existing itineraries and..
+            [] Edit them 
+            [] Save them 
+            [] Send them
+*/
+
+interface Itinerary {
+  _id: string;
+  cards: JSX.Element[];
+  userID: string;
+}
 function Manage() {
-  const [userData, setUserData] = useState({});
-  const [itineraries, setItineraries] = useState<object[]>([]);
-
+  const contextValue = useContext(UserContext) as unknown as {
+    user: UserData;
+    updateUser: (userData: UserData) => void;
+  };
+  const { user: userData, updateUser } = contextValue;
+  const [itineraries, setItineraries] = useState<Itinerary[]>([]);
   const { user } = useAuth0();
   const navigate = useNavigate();
 
@@ -26,36 +45,37 @@ function Manage() {
     }
   };
 
+  const getUser = async () => {
+    try {
+      const { data: userInfo } = await axios.get(
+        `/user/getUser/${user?.email}`
+      );
+      if (!userInfo) {
+        const newUser = {
+          email: user?.email,
+          nickname: user?.nickname,
+        };
+        const { data: newUserInfo } = await axios.post(
+          '/user/createUser',
+          newUser
+        );
+        updateUser(newUserInfo);
+
+        return;
+      } else {
+        const { data: userItineraries } = await axios.get(
+          `/user/getItineraries/${userInfo._id}`
+        );
+        setItineraries(userItineraries);
+        updateUser(userInfo);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     if (user) {
-      // eslint-disable-next-line no-inner-declarations
-      async function getUser() {
-        try {
-          const { data: userInfo } = await axios.get(
-            `/user/getUser/${user?.email}`
-          );
-          if (!userInfo) {
-            const newUser = {
-              email: user?.email,
-              nickname: user?.nickname,
-            };
-            const { data: newUserInfo } = await axios.post(
-              '/user/createUser',
-              newUser
-            );
-            setUserData(newUserInfo);
-            return;
-          } else {
-            const { data: userItineraries } = await axios.get(
-              `/user/getItineraries/${userInfo._id}`
-            );
-            setItineraries(userItineraries);
-            setUserData(userInfo);
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
       getUser();
     }
   }, [user]);
@@ -67,7 +87,7 @@ function Manage() {
       <div>
         All Itineraries:
         {itineraries.map((element) => {
-          return <div>{JSON.stringify(element)}</div>;
+          return <ul>{JSON.stringify(element)}</ul>;
         })}
       </div>
     </>
